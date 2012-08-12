3fa6080f6baa08c8b8b8b2642ffc2e620b93d30e
Matthias Brettschneider <frosch03@gmail.com>
2012-08-12 23:58:36 +0200

    added a navigation file

---
+++ Navigation.hs
@@ -0,0 +1,14 @@
+module Navigation
+where
+
+-- Extern
+import Text.XHtml.Strict hiding (sub)
+
+--   Blog Navigation
+-- -------------------
+--   * latest (by date)
+--   * this month (title by month this year (only this month))
+--   * last month  (title by month this year (only last month))
+--   * this year by month
+--   * all articles (all by subject)
+
